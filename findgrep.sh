8202a825c0035942216afd049a491f45f5e23f89
n20010 <n20010@std.it-college.ac.jp>
2020-06-24 11:54:00 +0900

    file pattern

--- findgrep.sh
+++ findgrep.sh
@@ -2,10 +2,16 @@
 
 pattern=$1
 directory=$2
+name=$3
 if [ -z "$directory" ]; then
 	directory="."
 fi
+
+if [ -z "$name" ]; then
+	name='*'
+fi
+
 # -n : print line number
 # -h : print the file name
-find . "$directory" -type f | xargs grep -nH "$pattern"
+find  "$directory" -type f -name "$name" | xargs grep -nH "$pattern"
 
